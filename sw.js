513d87cba5ca633dc943a8b196179ed1c4a3812a
thegrtnx <thegrtnx@duck.com>
2025-05-02 03:28:25 +0100

    🚧 WIP: updated

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 self.addEventListener("push", function (event) {
-	const data = event.data.json();
+	const data = event.data?.json() || { title: "No title", body: "No body" };
 	event.waitUntil(
 		self.registration.showNotification(data.title, {
 			body: data.body,
