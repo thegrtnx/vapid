b7c9b0a1a15fd6ee17675a12ef4d54beec5346dd
thegrtnx <thegrtnx@duck.com>
2025-05-02 03:31:48 +0100

    🚧 WIP: test vapid

--- sw.js
+++ sw.js
@@ -1,3 +1,11 @@
+self.addEventListener("install", (event) => {
+	console.log("Service Worker installing...");
+});
+
+self.addEventListener("activate", (event) => {
+	console.log("Service Worker activated.");
+});
+
 self.addEventListener("push", function (event) {
 	const data = event.data?.json() || { title: "No title", body: "No body" };
 	event.waitUntil(
